787aadf8f49d26f457c6f4637517839bda7797ca
hualin <hualinit@gmail.com>
2019-04-28 20:27:04 +0800

    代码优化

--- src/common/js/promiseDemo.js
+++ src/common/js/promiseDemo.js
@@ -58,3 +58,56 @@ class MyPromise {
         })
     }
 }
+
+
+// import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+// import "antd/dist/antd.css";
+// import "./index.css";
+// import { Select } from "antd";
+
+// function SearchInput() {
+//   const [options, setOptions] = useState([]);
+//   const [value, setValue] = useState(undefined);
+
+//   function handleChange(val) {
+//     setValue(val);
+//   }
+
+//   function handleSearch(val) {
+//     if (val === "a") {
+//       setTimeout(() => {
+//         setOptions([{ label: "a", value: "a" }]);
+//       }, 1000);
+//     } else {
+//       setOptions([]);
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <p>
+//         先输入“a”，查询出数据，选中；然后再次输入其他数据查询，no data,
+//         此时失去焦点，期望select为空
+//       </p>
+//       <Select
+//         style={{ width: 300 }}
+//         value={value}
+//         showSearch
+//         defaultActiveFirstOption={false}
+//         showArrow={false}
+//         filterOption={false}
+//         onSearch={handleSearch}
+//         onChange={handleChange}
+//       >
+//         {options.map(item => (
+//           <Select.Option key={item.value} value={item.value}>
+//             {item.label}
+//           </Select.Option>
+//         ))}
+//       </Select>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<SearchInput />, document.getElementById("container"));
